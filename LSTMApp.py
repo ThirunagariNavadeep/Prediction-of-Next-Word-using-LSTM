383e1a14561cc290c9502cc1081aff7b4ea256ca
ThirunagariNavadeep <thirunagarinavadeep4@gmail.com>
2025-09-27 13:16:33 +0530

    LSTMApp.py

--- LSTMApp.py
+++ LSTMApp.py
@@ -1,11 +1,3 @@
-import subprocess
-import sys
-
-print("🔎 Installed packages:")
-subprocess.run([sys.executable, "-m", "pip", "list"])
-
-
-
 import streamlit as st
 import numpy as np
 import pickle
@@ -35,6 +27,7 @@ if st.button("Predict Next Word"):
     max_sequence_len = model.input_shape[1]+1
     next_word = predict_next_word(model, tokenizer, input_text, max_sequence_len)
     st.write(f'Next Word:{next_word}')
+
 
 
 
